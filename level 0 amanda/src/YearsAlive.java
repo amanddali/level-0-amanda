beeb550a922caea08ff4754447511a62e88336e5
LEAUGE COMPUTER <DO NOT CHANGE THIS/>
2017-01-15 11:27:28 -0800

    used for and if loops

---
+++ level 0 amanda/src/YearsAlive.java
@@ -0,0 +1,11 @@
+
+public class YearsAlive {
+	public static void main(String[] args) {
+		for (int i = 2002; i <= 2017; i++) {
+			System.out.print(i);
+			if(i < 2017){
+			System.out.print(", ");
+			}
+		}
+	}
+}
