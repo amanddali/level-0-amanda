1484495875fde72166d1e85642e6d1e40c40be00
Kadorus <league@WTS10.attlocal.net>
2017-08-20 15:57:11 -0700

    throwback

--- level 0 amanda/src/CircleRing.java
+++ level 0 amanda/src/CircleRing.java
@@ -3,11 +3,12 @@ import org.jointheleague.graphical.robot.Robot;
 public class CircleRing {
 	public static void main(String[] args) throws Exception {
 		Robot bob = new Robot();
+		bob.setSpeed(10);
 		bob.setX(150);
 		bob.setY(200);
 		bob.penDown();
-		bob.setSpeed(10);
 		for (int i = 0; i < 360; i++) {
+			bob.setSpeed(10);
 			bob.move(3);
 			bob.turn(1);
 			if (i%20 == 0) {
