e6e53652f540c1c8e0f79a4c8661fc05050e82a7
amanddali <league@WTS22.attlocal.net>
2017-08-27 14:48:48 -0700

    just in case

--- level 0 amanda/src/Magic8Ball.java
+++ level 0 amanda/src/Magic8Ball.java
@@ -1,4 +1,27 @@
+// Copyright Wintriss Technical Schools 2013
+import java.util.Random;
+
+import javax.swing.JOptionPane;
 
 public class Magic8Ball {
-
+	
+	public static void main(String[] args) {
+		
+		int Random = new Random().nextInt(4);
+		System.out.println(Random);
+		JOptionPane.showInputDialog("You may ask a question");
+		if (Random == 0) {
+			JOptionPane.showMessageDialog(null, "Yes");
+		}
+		if (Random == 1) {
+			JOptionPane.showMessageDialog(null, "No");
+		}
+		if (Random ==2) {
+			JOptionPane.showMessageDialog(null, "Maybe you should ask Google?");
+		}
+		if (Random == 3) {
+			JOptionPane.showMessageDialog(null, "Ask again later");
+		}
+	}
 }
+
